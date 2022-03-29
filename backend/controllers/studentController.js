2e857bf51fda7b6489081912f78890e2efa3eabd
Supercoder536 <61321645+Supercoder536@users.noreply.github.com>
2022-03-29 12:49:20 +0530

    JWT

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -1,18 +1,23 @@
 const asynchandler = require("express-async-handler");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const studentSchema = require("../models/studentSchema.js");
+const jwt = require("jsonwebtoken");
+
+const createToken = (id) => {
+  return jwt.sign(id, process.env.JWT_SECRET);
+};
 //@desc     Register Student
 //@route    POST /api/students
 //@access   Public
 const registerStudent = asynchandler(async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !password || !email) {
-    res.status(404);
+    res.status(401);
     throw new Error("Invalid Request");
   }
   const userexsists = await studentSchema.findOne({ email });
   if (userexsists !== null) {
-    res.status(404);
+    res.status(401);
     throw new Error("User already exsists");
   }
   const salt = await bcrypt.genSalt(10);
@@ -22,14 +27,33 @@ const registerStudent = asynchandler(async (req, res) => {
     email,
     password: hashedpassword,
   });
-  res.status(200).json(newuser);
+  const token = createToken({ id: newuser._id.toString() });
+  res.status(200).json({ user: newuser, token });
 });
 
 //@desc     Login Student
 //@route    POST /api/students/login
 //@access   Public
 const loginStudent = asynchandler(async (req, res) => {
-  res.status(200).json({ message: "Login Student" });
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(401);
+    throw new Error("Invalid Request");
+  }
+  const user = await studentSchema.findOne({ email });
+  if (user === null) {
+    res.status(401);
+    throw new Error("No such user exsists");
+  }
+  if (!(await bcrypt.compare(password, user.password))) {
+    res.status(401);
+    throw new Error("Password does not match");
+  }
+  const token = createToken({ id: user._id.toString() });
+  res.status(200).json({
+    user: { id: user._id, name: user.name, email: user.email },
+    token: token,
+  });
 });
 
 //@desc     Get Student Data
